207353e4f31c86d12feef70e000849c05b5d9ec9
kawn <iamgoingtobealivetoday@gmail.com>
2026-01-11 00:58:23 +0200

    remove unnecessary imports (pynput stays for now)

--- APP/MainUI.py
+++ APP/MainUI.py
@@ -17,29 +17,6 @@ from PySide6.QtWidgets import (QComboBox, QFrame, QGridLayout,
     QStackedWidget, QWidget)
 from pynput import keyboard as pynput_keyboard
 
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'DeepMacrooOgjiN.ui'
-##
-## Created by: Qt User Interface Compiler version 6.10.1
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QGridLayout,
-    QLabel, QLineEdit, QMainWindow, QPlainTextEdit,
-    QPushButton, QScrollArea, QSizePolicy, QSpacerItem,
-    QStackedWidget, QWidget)
-import assets_rc
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
