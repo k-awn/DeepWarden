2a12b9d101448004bb94a643b1218f92b8de6f87
kawn <iamgoingtobealivetoday@gmail.com>
2026-02-23 01:16:17 +0200

    set starting page to save menu

--- APP/MainUI.py
+++ APP/MainUI.py
@@ -3200,7 +3200,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(1)
+        self.stackedWidget.setCurrentIndex(6)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
